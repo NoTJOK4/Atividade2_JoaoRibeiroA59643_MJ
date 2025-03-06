80b1ff07d0f9a3eaf8376c91af5e1ac6f3f0b7ff
NoTJOK4 <joao.miguel.ribeiro05@gmail.com>
2025-03-06 00:42:43 +0000

    Game environment and physics

--- Assets/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS/PlayerController.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControllerTutorialUpdates : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
-    public InputAction MoveAction;
+  public InputAction MoveAction;
+  Rigidbody2D rigidbody2d;
+  Vector2 move;
 
-    void Start()
-    {
 
-        MoveAction.Enable();
-    }
+  void Start()
+  {
+     MoveAction.Enable();
+     rigidbody2d = GetComponent<Rigidbody2D>();
+  }
+ 
+  void Update()
+  {
+     move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move);
+  }
 
-    void Update()
-    {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
 
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-
-        transform.position = position;
-    }
+  void FixedUpdate()
+  {
+     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+     rigidbody2d.MovePosition(position);
+  }
 }
